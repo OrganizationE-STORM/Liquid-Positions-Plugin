7872a8beb7c1884aa1ad4cf2d681f88d9b1f7b35
nicolova-tr <nicolova@etoro.com>
2026-01-26 20:22:53 +0100

    Removed cache and enabled viaIR options in hardhat

--- contracts/LPPlugin.sol
+++ contracts/LPPlugin.sol
@@ -44,11 +44,6 @@ contract LPPlugin is AbstractPlugin, IERC721Receiver {
         uint160 price;
     }
 
-    struct CacheAmountsOnERC721Received {
-        uint256 amount0;
-        uint256 amount1;
-    }
-
     /// @notice Default plugin configuration flag - includes position hooks, swap hooks, and dynamic fee capability
     uint8 public constant override defaultPluginConfig =
         uint8(
@@ -66,7 +61,6 @@ contract LPPlugin is AbstractPlugin, IERC721Receiver {
     /// @notice Plugin fee in PPM (parts per million, e.g., 10000 = 1%)
     uint24 public pluginFeeRate;
     Cache private _cache;
-    CacheAmountsOnERC721Received private _cacheAmountsOnERC721Received;
 
     address public immutable callback;
 
@@ -263,7 +257,7 @@ contract LPPlugin is AbstractPlugin, IERC721Receiver {
         );
 
         // Decrease liquidity from user's latest NFT
-        (_cacheAmountsOnERC721Received.amount0, _cacheAmountsOnERC721Received.amount1) = INonfungiblePositionManager(msg.sender)
+        (uint256 amount0, uint256 amount1) = INonfungiblePositionManager(msg.sender)
             .decreaseLiquidity(
                 INonfungiblePositionManager.DecreaseLiquidityParams({
                     tokenId: tokenId,
@@ -275,22 +269,22 @@ contract LPPlugin is AbstractPlugin, IERC721Receiver {
             );
 
         // Collect tokens from the NFT
-        (_cacheAmountsOnERC721Received.amount0, _cacheAmountsOnERC721Received.amount1) = INonfungiblePositionManager(msg.sender).collect(
+        (amount0, amount1) = INonfungiblePositionManager(msg.sender).collect(
             INonfungiblePositionManager.CollectParams({
                 tokenId: tokenId,
                 recipient: address(this),
-                amount0Max: uint128(_cacheAmountsOnERC721Received.amount0),
-                amount1Max: uint128(_cacheAmountsOnERC721Received.amount1)
+                amount0Max: uint128(amount0),
+                amount1Max: uint128(amount1)
             })
         );
 
         // Approve tokens for reinvestment
         require(
-            IERC20(IAlgebraPool(pool).token0()).approve(callback, _cacheAmountsOnERC721Received.amount0),
+            IERC20(IAlgebraPool(pool).token0()).approve(callback, amount0),
             "Token0 approval failed"
         );
         require(
-            IERC20(IAlgebraPool(pool).token1()).approve(callback, _cacheAmountsOnERC721Received.amount1),
+            IERC20(IAlgebraPool(pool).token1()).approve(callback, amount1),
             "Token1 approval failed"
         );
 
@@ -300,7 +294,7 @@ contract LPPlugin is AbstractPlugin, IERC721Receiver {
             ? ILPToken(lpTokenAddress).balanceOf(address(this))
             : 0;
 
-        ILPCallback(callback).mint(from, tickLower, tickUpper, _cacheAmountsOnERC721Received.amount0, _cacheAmountsOnERC721Received.amount1);
+        ILPCallback(callback).mint(from, tickLower, tickUpper, amount0, amount1);
 
         require(
             ILPToken(lpTokenByTicks[tickLower][tickUpper]).transfer(
