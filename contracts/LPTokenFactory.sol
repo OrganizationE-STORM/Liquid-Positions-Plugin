47adcc1848eef84789ea5424f120240ff693e868
nicolova-tr <nicolova@etoro.com>
2026-02-23 23:08:39 +0100

    F-2026-15129 | Redundant Empty Constructor

--- contracts/LPTokenFactory.sol
+++ contracts/LPTokenFactory.sol
@@ -5,8 +5,6 @@ import {ILPTokenFactory} from "./interfaces/ILPTokenFactory.sol";
 import {LPToken} from "./LPToken.sol";
 
 contract LPTokenFactory is ILPTokenFactory {
-    constructor() {}
-
     function create(string memory name, string memory symbol) external returns(address) {
         LPToken token = new LPToken(name, symbol);
         token.transferOwnership(msg.sender);
